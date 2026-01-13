507921a5b4028b770aa48c01acb6bc99a073b9df
leedongzheo <125799135+leedongzheo@users.noreply.github.com>
2026-01-13 23:48:48 +0700

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -19,11 +19,13 @@ GLOBAL_MIN_AREA_DEFAULT = 143.5
 # NORM_MEAN = [0.1608, 0.1751, 0.1216]
 # NORM_STD  = [0.2526, 0.2466, 0.1983]
 # Ảnh 3 channel grayscale-CLAHE-ComboGamma_CLAHE 640x640
-# NORM_MEAN = [0.2512, 0.2741, 0.1900]
-# NORM_STD = [0.2775, 0.2605, 0.2203]
+if "args.augment":
+	NORM_MEAN = [0.2512, 0.2741, 0.1900]
+	NORM_STD = [0.2775, 0.2605, 0.2203]
 # Ảnh 3 channel grayscale 640x640
-NORM_MEAN = [0.2512, 0.2512, 0.2512]
-NORM_STD = [0.2775, 0.2775, 0.2775]
+else: 
+	NORM_MEAN = [0.2512, 0.2512, 0.2512]
+	NORM_STD = [0.2775, 0.2775, 0.2775]
 # --- HELPER FUNCTIONS ---
 def get_clean_breast_mask(image):
     if len(image.shape) == 3:
