3e421b431b3c317e17e13ea54b877ffdfe64bcbf
leedongzheo <125799135+leedongzheo@users.noreply.github.com>
2026-01-22 18:25:09 +0700

    Update train.py

--- train.py
+++ train.py
@@ -107,12 +107,20 @@ def main(args):
     #         classes=1,
     #         drop_path_rate=0.2
     # )
-    model = smp.UnetPlusPlus(
+#     model = smp.UnetPlusPlus(
+#         encoder_name="tu-resnest50d", 
+#         encoder_weights="imagenet",
+#         in_channels=3,
+#         classes=1,
+#         drop_path_rate=0.5
+# )
+    model = smp.Unet(
         encoder_name="tu-resnest50d", 
         encoder_weights="imagenet",
         in_channels=3,
         classes=1,
-        drop_path_rate=0.5
+        # drop_path_rate nên được đưa vào encoder_params để truyền xuống backbone timm
+        encoder_params={"drop_path_rate": 0.5} 
 )
     # 2. Khởi tạo Optimizer
     opt = optimizer_module.optimizer(model=model) 
