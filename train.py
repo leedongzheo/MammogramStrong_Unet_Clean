5c9391e5d785eb56fefe372fc94efcaf363b391d
leedongzheo <125799135+leedongzheo@users.noreply.github.com>
2026-01-26 01:17:18 +0700

    Update train.py

--- train.py
+++ train.py
@@ -177,7 +177,7 @@ def main(args):
         encoder_weights="imagenet",
         in_channels=3,
         classes=1,
-        drop_path_rate = 0.3,
+        drop_path_rate = 0.2,
         decoder_attention_type="scse"  # Giữ nguyên attention nếu bạn muốn
     )
 #     ## force_drop_path_linear(model, 0.5) => Sai
